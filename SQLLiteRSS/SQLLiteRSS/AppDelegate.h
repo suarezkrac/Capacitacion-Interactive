22fdd0c38854bdcf3eaec3e04a141ba2a87dc9aa
Giovanny Suarez <suarezkrac@gmail.com>
2013-07-31 19:03:05 -0500

    NewFiles

---
+++ SQLLiteRSS/SQLLiteRSS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SQLLiteRSS
+//
+//  Created by Equipo Desarrollo 2 on 31/07/13.
+//  Copyright (c) 2013 Cymetria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
